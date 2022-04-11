bf707f3fb9c8d7949ff0aecd075178222a6dac4e
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-04-11 22:56:12 +0600

    new code

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,10 +1,23 @@
 import { useState } from "react";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import app from "../firebase.init";
+
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
 const useFirebase = () => {
   const [user, setUser] = useState({});
+
   const signInWithGoogle = () => {
-    console.log("hello");
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch((error) => console.error(error));
   };
+
   return {
     user,
     signInWithGoogle,
