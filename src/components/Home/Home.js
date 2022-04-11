70599500f50daeac00642647ef4c2f76b311d492
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-04-12 00:02:44 +0600

    new code

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
+import useFirebase from "../../hooks/useFirebase";
 
 const Home = () => {
+  const { user } = useFirebase();
   return (
     <div>
       <h4>this is home</h4>
+      <p>Current user is: {user ? user.displayName : "No body"}</p>
     </div>
   );
 };
