5a7ae79830e52919accf04aa8ced2a5490710352
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-04-12 00:05:08 +0600

    new code

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,9 +1,12 @@
 import React from "react";
+import useFirebase from "../../hooks/useFirebase";
 
 const Products = () => {
+  const { user } = useFirebase();
   return (
     <div>
       <h4>this is products</h4>
+      <p>{user ? user.displayName : "no body"}</p>
     </div>
   );
 };
