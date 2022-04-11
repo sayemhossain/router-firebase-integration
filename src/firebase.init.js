ee132e9161169245e61b7c012ae6eb2a3ade9ec3
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-04-11 17:23:31 +0600

    firebase setup done

---
+++ src/firebase.init.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAE7i6UDJ8LUzyE9ToyHk6BLrcWLPVjNm0",
+  authDomain: "router-firebase-integrat-bc830.firebaseapp.com",
+  projectId: "router-firebase-integrat-bc830",
+  storageBucket: "router-firebase-integrat-bc830.appspot.com",
+  messagingSenderId: "388846056150",
+  appId: "1:388846056150:web:5cf748ce82d5adaacb8f77",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
