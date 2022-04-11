96df350535986ac68b1d8671394f212f7dfe232b
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-04-12 02:53:00 +0600

    private router

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Login from "./components/Login/Login";
 import Products from "./components/Products/Products";
 import Orders from "./components/Orders/Orders";
 import Register from "./components/Register/Register";
+import RequireAuth from "./components/RequireAuth/RequireAuth";
 
 function App() {
   return (
@@ -14,7 +15,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/products" element={<Products></Products>}></Route>
-        <Route path="/orders" element={<Orders></Orders>}></Route>
+        <Route
+          path="/orders"
+          element={
+            <RequireAuth>
+              <Orders></Orders>
+            </RequireAuth>
+          }
+        ></Route>
         <Route path="/register" element={<Register></Register>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
       </Routes>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase.init";
 
 const Login = () => {
   const [signInWithGoogle, user] = useSignInWithGoogle(auth);
+  const location = useLocation();
+  const navigate = useNavigate();
+  let from = location.state?.from?.pathname || "/";
+
+  const handleGoogleSignIn = () => {
+    signInWithGoogle().then(() => {
+      navigate(from, { replace: true });
+    });
+  };
+
   return (
     <div>
       <h4>Please login</h4>
@@ -18,10 +28,7 @@ const Login = () => {
         <input type="submit" value="Login" />
       </form>
       <br></br>
-      <button
-        onClick={() => signInWithGoogle()}
-        style={{ marginRight: "20px" }}
-      >
+      <button onClick={handleGoogleSignIn} style={{ marginRight: "20px" }}>
         Google SignIn
       </button>
       <button>Github SignIn</button>
