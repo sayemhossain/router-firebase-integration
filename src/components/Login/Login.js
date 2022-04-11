03c4865218afe48b4228281b4b1fd1f5ba837fa0
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-04-11 21:33:47 +0600

    new code added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <div>
       <h4>Please login</h4>
+      <form>
+        <input type="email" placeholder="Your email" />
+        <br></br>
+        <br></br>
+        <input type="password" name="" id="" placeholder="Your password" />
+        <br></br>
+        <br></br>
+        <button>Login</button>
+      </form>
+      <p>
+        Don't have account? <Link to="/register">Register Now</Link>
+      </p>
     </div>
   );
 };
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Reviews = () => {
   return (
     <div>
-      <h3>This is Register section</h3>
+      <h3>Registation Form</h3>
+      <form>
+        <input type="text" placeholder="Your name" />
+        <br></br>
+        <br></br>
+        <input type="email" placeholder="Your email" />
+        <br></br>
+        <br></br>
+        <input type="password" name="" id="" placeholder="Your password" />
+        <br></br>
+        <br></br>
+        <button>Register Now</button>
+      </form>
+      <p>
+        Already have account? Go to <Link to="/login">Login</Link> page
+      </p>
     </div>
   );
 };
