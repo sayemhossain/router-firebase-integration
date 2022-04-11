558465df96ed9dd3379036b085dad758e287f2cb
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-04-11 17:30:51 +0600

    new code added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
